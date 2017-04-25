cab9c8af3c796b7878bb2ef50340a12cc2875ad1
wuzi <549243104@qq.com>
2017-04-25 17:17:55 +0800

    router-view未注册bug fixed

--- app/router/router.js
+++ app/router/router.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// Vue.use(VueRouter)
 
 const routes = [
   {
   	path: '/first', component: require('../index/components/First.vue')
   },{
   	path: '/second', component: require('../index/components/Second.vue')
-  },{
-  	path:'/',redirect:'/first'
-  }
+  },
+  // {
+  // 	path:'*',redirect:'/first'
+  // }
 ]
 
 export default routes;
