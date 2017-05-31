bc2a1695e9521fccde2c80d61ff11c88626527e0
wuzi <549243104@qq.com>
2017-05-31 17:31:08 +0800

    研究h5 filereader

--- app/index/module/reservefive/reserveFive.vue
+++ app/index/module/reservefive/reserveFive.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-      富文本研究：
+      富文本研究（wangeditor）
+      <div id='div1'>
+
+
+      </div>
   </div>
 </template>
 
@@ -10,7 +14,22 @@ export default {
     return {
     }
   },
+  mounted: function(){
+    // this.test1()
+    var editor = new wangEditor('div1');
+    editor.create();
+  },
   computed:{
+  },
+  methods:{
+    test1(){
+      console.log('come in')
+    //  let a = function* f(){
+    //     console.log('1');
+    //     yield console.log('2')
+    //     console.log('3')
+    //   }
+    }
   },
   components: {
   }
--- app/index/module/reservefour/reserveFour.vue
+++ app/index/module/reservefour/reserveFour.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-      预留4
-     <!-- 路由匹配到的组件将渲染在这里 -->
-       <router-view></router-view>
+      多图片上传
+      <input type="file" id="file" multiple/>
+      <div id="result"></div>
+      <input type="button" value="读取图像" @click="readAsDataURL()" />
+      <input type="button" value="读取二进制数据" @click="readAsBinaryString" />
+      <input type="button" value="读取文本文件" @click="readAsText()" />   
   </div>
 </template>
 
@@ -13,6 +16,56 @@ export default {
     }
   },
   computed:{
+  },
+  mounted: function(){
+    
+  },
+  methods:{
+    change(){
+      var input = document.getElementById('file_input');
+
+    },
+    readAsBinaryString(){  
+      alert('123')
+      var file = document.getElementById("file").files[0];  
+      var reader = new FileReader();  
+      //将文件以二进制形式读入页面  
+      reader.readAsBinaryString(file);  
+      reader.onload=function(f){  
+          var result=document.getElementById("result");  
+          console.log(this.result)
+          //显示文件  
+          result.innerHTML=this.result;  
+      }  
+    },
+    readAsDataURL(){  
+        //检验是否为图像文件  
+        var file = document.getElementById("file").files[0];  
+        if(!/image\/\w+/.test(file.type)){  
+            alert("看清楚，这个需要图片！");  
+            return false;  
+        }  
+        var reader = new FileReader();  
+        //将文件以Data URL形式读入页面  
+        reader.readAsDataURL(file);  
+        reader.onload=function(e){  
+            var result=document.getElementById("result");  
+            //显示文件  
+            result.innerHTML='<img src="' + this.result +'" alt="" />';  
+        }  
+    },
+    readAsText(){  
+        var file = document.getElementById("file").files[0];  
+        var reader = new FileReader();  
+        //将文件以文本形式读入页面  
+        reader.readAsText(file);  
+        reader.onload=function(f){  
+            var result=document.getElementById("result");  
+            //显示文件  
+            result.innerHTML=this.result;  
+        }  
+    }, 
+
   },
   components: {
   }
