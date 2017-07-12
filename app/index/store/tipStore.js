3b8e0b79a830d0a16153eae936d212c53936f9e5
wuzi <549243104@qq.com>
2017-07-12 11:02:30 +0800

    add

---
+++ app/index/store/tipStore.js
@@ -0,0 +1,22 @@
+const tipStore = {
+    state:{
+
+    },
+    actions:{
+        setTip({commit}, parqams){
+
+        }
+    },
+    mutations:{
+        SET_TIP: (state, param) => {
+
+        }
+    },
+    getters:{
+        getTip: (state) => {
+            
+        }
+    }
+}
+
+export default tipStore
