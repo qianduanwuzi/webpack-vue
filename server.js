69dcd31b45d05b96fd75c271347c29ae26fb70bc
wuzi <549243104@qq.com>
2017-07-29 17:20:01 +0800

    icon download to local

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 
-var config = require("./webpack.config.js");
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
+// var config = require("./webpack.config.js");
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
 
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:9966/");
+// config.entry.app.unshift("webpack-dev-server/client?http://localhost:9966/");
 
-var compiler = webpack(config);
-var server = new WebpackDevServer(compiler, {
-    publicPath: config.output.publicPath,
-    stats: {
-        colors: true,
-    }
+// var compiler = webpack(config);
+// var server = new WebpackDevServer(compiler, {
+//     publicPath: config.output.publicPath,
+//     stats: {
+//         colors: true,
+//     }
+// });
+// server.listen(9966, function(){
+//     console.log('listening at http://localhost:9966');
+// });
+
+
+var express = require("express");
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config.js");
+
+var app = express();
+var compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath // 大部分情况下和 `output.publicPath`相同
+}));
+
+app.listen(9966, function () {
+  console.log("Listening on port 9966!");
 });
-server.listen(9966, function(){
-    console.log('listening at http://localhost:9966');
-});
+
+
+
+
 
 // const Vue = require('vue')
 // const server = require('express')()
